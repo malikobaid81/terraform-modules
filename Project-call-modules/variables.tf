b249c18ac36770e249c00decd7db605c0529e83e
Malik <malikobaid81@hotmail.com>
2024-09-12 06:02:40 -0700

    created nat gateway

---
+++ Project-call-modules/variables.tf
@@ -0,0 +1,35 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "project_name" {
+  default = "test"
+}
+
+variable "environment" {
+  default = "dev"
+}
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_az1_cidr" {
+  default = "10.0.0.0/24"
+}
+
+variable "public_subnet_az2_cidr" {
+  default = "10.0.1.0/24"
+}
+
+variable "private_app_subnet_az1_cidr" {
+  default = "10.0.2.0/24"
+}
+variable "private_app_subnet_az2_cidr" {
+  default = "10.0.3.0/24"
+}
+variable "private_data_subnet_az1_cidr" {
+  default = "10.0.4.0/24"
+}
+variable "private_data_subnet_az2_cidr" {
+  default = "10.0.5.0/24"
+}
